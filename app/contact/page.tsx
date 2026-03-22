780d335c41ba26f0631019d26bd84b15530634c5
Uche Brendan <3mekat5@gmail.come>
2026-03-22 07:27:15 +0100

    mobile view adjustments

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -5,15 +5,15 @@ import Link from "next/link"
 
 const page = () => {
   return (
-    <div className="flex min-h-screen w-full items-center justify-center text-center">
+    <div className="flex min-h-screen w-full items-center justify-center">
       <div className="absolute top-5 left-5">
         <Link href={"/"} className={buttonVariants({ variant: "outline" })}>
           Go Back
         </Link>
       </div>
-      <div className="flex h-full w-full flex-col items-center justify-center gap-8">
-        <div>
-          <h1 className="text-3xl font-bold ">CONTACT REMY SNEEZE⭐</h1>
+      <div className="flex h-full w-full flex-col items-center justify-center gap-8 text-center">
+        <div className="w-96">
+          <h1 className="text-3xl font-bold max-sm:text-2xl">CONTACT REMY SNEEZE⭐</h1>
           <p>Send a message, fan mail, suggestion, review etc...</p>
         </div>
         <ContactForm />
